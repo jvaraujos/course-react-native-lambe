cb5b5a78cd42eac386d0b558e9c55247b6f38d6f
jvaraujos <39956177+jvaraujos@users.noreply.github.com>
2022-02-03 11:11:16 -0300

    configurando projeto

--- lambe/src/App.js
+++ lambe/src/App.js
@@ -1,16 +1,18 @@
-import React,{Component} from 'react'
-import { SafeAreaView,Text,StyleSheet } from 'react-native'
+import React, { Component } from 'react'
+import { SafeAreaView, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
-export default class App extends Component{
-    render (){
+export default class App extends Component {
+    render() {
         return (
-        <SafeAreaView style={styles.container}>
-        <Text>App</Text>
-        </SafeAreaView>
-     )
-}
+            <SafeAreaView style={styles.container}>
+
+            </SafeAreaView>
+        )
+    }
 }
 const styles = StyleSheet.create({
-    container:{
+    container: {
 
-     }})
+    }
+})
+
